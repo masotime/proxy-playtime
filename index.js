2d5334fdbed295dcf28dcc79c8ede49f7b2c565b
vinnie garcia <vingarcia@paypal.com>
2016-05-27 13:57:37 -0700

    fix (test/mocks): show how to mock

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-require('babel-register');
-console.log('+'.repeat(50));
-console.log('Privacy proxy: ');
-require('./src/privacy/privacy.test');
-console.log('+'.repeat(50));
-console.log('Dynamic properties: ');
-require('./src/dynamic-property/dynamic-property.test');
+console.error('Use the command `npm start` to run the examples/tests');
+process.exit(1);
